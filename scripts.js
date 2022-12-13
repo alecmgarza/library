d0b79224ce6bb44cfd2fe8115cc06f854362ec3a
Alec Garza <alecmgarza@gmail.com>
2022-12-12 21:38:11 -0600

    Use a function to take form input and create book object, add new book to array.

--- scripts.js
+++ scripts.js
@@ -7,10 +7,6 @@ function Book(title, author, pages, beenRead) {
     this.beenRead = beenRead
 }
 
-function addBookToLibrary(newBook) {
-    myLibrary.push(newBook);
-}
-
 function displayBooks() {
     for (let i = 0; i < myLibrary.length; i++) {
         const div = document.createElement('div');
@@ -26,6 +22,18 @@ btn.onclick = function() {
     form.style.display = 'block';
 }
 
+form.onsubmit = function(event) {
+    event.preventDefault();
+
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let beenRead = document.getElementById('beenRead').value;
+
+    let newBook = new Book(title, author, pages, beenRead)
+    myLibrary.push(newBook);
+}
+
 const closeForm = document.querySelector('button#closeForm');
 
 closeForm.onclick = function() {
@@ -33,3 +41,7 @@ closeForm.onclick = function() {
 }
 
 displayBooks();
+
+// figure out how to display array of objects using JSON.stringify
+// how does that work with my for loop?
+// figure out how to refresh display of objects when they are added or removed
